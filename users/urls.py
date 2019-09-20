6201d4809ddcbc272de195e098df352ee113f3d2
lzk <1005826614@qq.com>
2019-09-20 11:40:35 +0800

    注释出错

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,6 @@ from django.contrib.auth.views import login
 from . import views
 
 urlpatterns=[
-    #登录
     url(r'^login/$', login, {'template_name':'users/login.html'}, name='login'),
-    # 注销
     url(r'^login/$', views.logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -6,6 +6,5 @@ from django.contrib.auth import logout
 
 # Create your views here.
 def logout_view(request):
-    """注销用户"""
     logout(request)
     return HttpResponseRedirect(reverse('learning_logs:index'))
