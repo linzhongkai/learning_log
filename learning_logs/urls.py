f4799b82320d3b903e79b73bf8141535d8b0b9b0
lzk <1005826614@qq.com>
2019-09-20 11:38:11 +0800

    add log_out

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
     url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 
+
 ]
--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,8 @@ from django.contrib.auth.views import login
 from . import views
 
 urlpatterns=[
+    #登录
     url(r'^login/$', login, {'template_name':'users/login.html'}, name='login'),
+    # 注销
+    url(r'^login/$', views.logout_view, name='logout'),
 ]
