a556f8d3b8a6ff64b75a1718f18bc4f00ff69cff
lzk <1005826614@qq.com>
2019-09-20 15:20:37 +0800

    用户管理

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,21 +3,26 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     topics = Topic.objects.order_by('data_added')
     context = {'topics':topics}
     return render(request, 'learning_logs/topics.html', context)
+
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
@@ -30,6 +35,7 @@ def new_topic(request):
     context = {'form':form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
 
@@ -46,6 +52,7 @@ def new_entry(request, topic_id):
     context = {'topic':topic, 'form':form}
     return render(request, 'learning_logs/new_entry.html',context)
 
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
