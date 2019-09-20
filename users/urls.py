4b634df97524cc3ff66b6d5b06fb2a62f6b046c9
lzk <1005826614@qq.com>
2019-09-20 10:44:38 +0800

    add urls

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from django.contrib.auth.views import login
+from . import views
+urlpatterns=[
+    url(r'^login/$', login, {'template_name':'users/login.html'})
+
+]
