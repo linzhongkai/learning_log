19416053b99c96d5dfcffad3f471ff58e57e7c43
lzk <1005826614@qq.com>
2019-09-20 11:49:18 +0800

    注释尝试

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
+# -*- coding:utf-8 -*-
+
 from django.conf.urls import url
 from django.contrib.auth.views import login
 from . import views
 
 urlpatterns=[
     url(r'^login/$', login, {'template_name':'users/login.html'}, name='login'),
+    #注销
     url(r'^login/$', views.logout_view, name='logout'),
 ]
